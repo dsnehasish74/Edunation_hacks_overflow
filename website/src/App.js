d3725d9ee9471cd65be876b333670511b391de06
Malay Gain <malaygain10@gmail.com>
2021-04-10 15:07:25 +0530

    landing page added

--- website/src/App.js
+++ website/src/App.js
@@ -7,12 +7,14 @@ import {
 import RoomContainer from './components/room_container'
 import Home from './components/Home'
 import Profile from './components/profile'
+import Landing from './components/landing'
 function App() {
   return (
     <div className="App">
         <Router>
         <Switch>
         <Route exact path="/" component={Home}/>
+        <Route path="/landing" component={Landing}/>
         <Route path="/classroom/:id" component={RoomContainer}/>
         <Route path="/profile" component={Profile}/>
         <Redirect to="/"/>
